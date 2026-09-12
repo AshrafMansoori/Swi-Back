ca4119caa1313d586e10e46dae7ea9086fb364ee
Ashraf <mansooriashraf354@gmail.com>
2026-09-12 17:48:39 +0530

    Create ApiRespone ApiError And other Utility discussions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./database/db.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path:"./env"
@@ -7,3 +8,11 @@ dotenv.config({
 
 
 connectDB()
+.then(
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`   Server is Listenn On Port No.. ${process.env.PORT}`)
+    })
+)
+.catch((error)=>{
+    console.log("Error Occur When Connecting with DataBase ",error)
+})
